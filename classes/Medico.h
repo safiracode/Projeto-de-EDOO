f65ab6e0023e282c797ba15ab9e9d1e941a994f3
safiracode <safira.moraes.dev@gmail.com>
2025-11-04 16:14:15 -0300

    alterações

--- classes/Medico.h
+++ classes/Medico.h
@@ -3,6 +3,10 @@
 #include "FilaAtendimento.h" // Incluímos a definição da Fila
 #include <string>
 
+/*
+ * Classe Derivada. Herda de 'Pessoa' e adiciona
+ * atributos específicos do Médico (crm, especialidade).
+ */
 class Medico: public Pessoa {
 private:
     std::string crm;
@@ -11,11 +15,20 @@ private:
     FilaAtendimento filaDeAtendimento;
 
 public:
+    /*
+     * Construtor: Coleta dados para si e para a classe base.
+     * Repassa 'nome' e 'idade' para o construtor 'Pessoa'.
+     */
     Medico(std::string nome, int idade, std::string crm, std::string especialidade);
-
+    /*
+     * Setters: Validam e definem os atributos de Medico.
+     * 'setCRM' e 'setEspecialidade' rejeitam strings vazias.
+     */
     void setCRM(std::string crm);
     void setEspecialidade (std::string especialidade);
-
+    /*
+     * Getters: Fornecem acesso de leitura aos atributos de Medico.
+     */
     std::string getCRM() const;
     std::string getEspecialidade() const;
 
@@ -26,6 +39,18 @@ public:
     const FilaAtendimento& getFila() const; // Versão 'const'
 
     // Serialização JSON
+        /*
+     * Serialização Polimórfica (Override):
+     * Substitui 'toJSONString' de 'Pessoa'.
+     * Sua implementação irá *primeiro* chamar 'Pessoa::toJSONString()'
+     * e *depois* adicionar 'crm' e 'especialidade' ao JSON.
+     */
     std::string toJSONString() const override;
+        /*
+     * Método de Fábrica Estático (Desserialização):
+     * 'static' significa que é chamado na classe (Medico::fromJSONString)
+     * e não em um objeto. Converte uma string JSON de volta
+     * em um novo objeto 'Medico*'. Usado para carregar dados do disco.
+     */
     static Medico* fromJSONString(const std::string& jsonStr);
 };
